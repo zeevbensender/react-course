870f1998bbdc61034ba45b80555d5745d93a333d
lupo <bensender@gmail.com>
2025-01-06 15:02:12 +0000

    Exercise 1 is done

--- 01-01-first-app/01-starting-project/src/App.jsx
+++ 01-01-first-app/01-starting-project/src/App.jsx
@@ -1,14 +1,23 @@
+import MyGoal from "./MyGoal";
+function Header() {
+  return (
+    <header>
+    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <h2>Hello React Header!!!</h2>
+    <p>
+      Fundamental React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header/>
+      <MyGoal/>
       <main>
         <h2>Time to get started!</h2>
       </main>
---
+++ 01-01-first-app/01-starting-project/src/MyGoal.jsx
@@ -0,0 +1,12 @@
+function MyGoal(){
+    return(
+        <div>
+            <p class="centered-text">
+            
+            My main goal: Learn react from the ground up
+            </p>
+        </div>
+    );
+}
+
+export default MyGoal;
