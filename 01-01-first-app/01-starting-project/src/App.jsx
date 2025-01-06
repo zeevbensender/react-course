6a64c205b2df4302460f6d9daf4e0819d8828b72
lupo <bensender@gmail.com>
2025-01-06 15:37:12 +0000

    Second exercise is done

--- 01-01-first-app/01-starting-project/src/App.jsx
+++ 01-01-first-app/01-starting-project/src/App.jsx
@@ -1,12 +1,18 @@
 import MyGoal from "./MyGoal";
+const reactDescriptions = ["Core", "Crucial", "Fundamental"];
+
+function getRandomInt(max){
+  return Math.floor(Math.random() * (max + 1));
+}
 function Header() {
+  const description = reactDescriptions[getRandomInt(2)]
   return (
     <header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <h2>Hello React Header!!!</h2>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {description} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>
